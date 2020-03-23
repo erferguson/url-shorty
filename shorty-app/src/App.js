c07115ba5bee6e11c37f93ac385b0a8b3ded9893
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-03-23 15:47:49 -0700

    footer > set logo, rendering

--- shorty-app/src/App.js
+++ shorty-app/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 
 import Navigation from "./components/NavComponents/Navigation";
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Footer /> 
     </div>
   );
 }
---
+++ shorty-app/src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+
+import "../../App.css"
+import Logo from "../NavComponents/Shortly"
+// import { Fbook, Twitter, Instagram, Pinterest } from "../social"
+
+function Footer(){
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/"><Logo/></Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </Router>
+    )
+}
+export default Footer;
